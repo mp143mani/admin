463c1406e41ae4254a2a21f5b98da1e0bf7f3cc2
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-06-06 16:06:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import Sidebar from "./components/Sidebar";
-import Dashboard from "./components/Dashboard";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import AddUser from "./components/AddUser";
-import Profile from "./components/Profile";
-import PendingRequest from "./components/PendingRequest";
-import Task from "./components/Task";
-import ProfileDetails from "./components/ProfileDetails";
-import ResetPassword from "./components/ResetPassword";
+
+import Sidebar from './components/Sidebar';
+import Dashboard from './components/Dashboard';
+
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <div id="wrapper">
-        <Sidebar />
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/addUser" element={<AddUser />} />
-          <Route path="/profile" element={<Profile />}>
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="reset-password" element={<ResetPassword />} />
-          </Route>
-          <Route path="/pendingRequest" element={<PendingRequest />} />
-          <Route path="/task" element={<Task />} />
-          <Route path="*" element={<Navigate to="/dashboard" />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div id="wrapper">
+      <Sidebar/>
+      <Dashboard/>
+      
+      
+    </div>
   );
 }
 
