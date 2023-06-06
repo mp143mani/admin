99f0e0fd9783f077fc67940738f13cdc479c637e
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-06-06 16:07:58 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,81 +1,45 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import React from 'react'
 
 function Sidebar() {
-    let navigate= useNavigate()
-    return (
+    return <>
         <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            {/* Sidebar - Brand */}
-            <div className="sidebar-brand d-flex align-items-center justify-content-center">
+            {/* <!-- Sidebar - Brand --> */}
+            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div className="sidebar-brand-icon rotate-n-15">
                     <i className="fas fa-laugh-wink"></i>
                 </div>
                 <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-            </div>
+            </a>
 
-            {/* Divider */}
+            {/* <!-- Divider --> */}
             <hr className="sidebar-divider my-0" />
 
-            {/* Nav Item - Dashboard */}
-            <Link to='/dashboard'>
-                <li className="nav-item active">
-                    <div className="nav-link">
+            {/* <!-- Nav Item - Dashboard --> */}
+            <li className="nav-item active">
+                <a className="nav-link" href="index.html">
                     <i className="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span>
-                    </div>
-                </li>
-            </Link>
+                    <span>Dashboard</span></a>
+            </li>
 
-            {/* Divider */}
+            {/* <!-- Divider --> */}
             <hr className="sidebar-divider" />
 
-            {/* Heading */}
+            {/* <!-- Heading --> */}
             <div className="sidebar-heading">
                 Interface
             </div>
 
-            {/* Nav Item - Pages Collapse Menu */}
-            <Link to='/addUser'>
-                <li className="nav-item">
-                    <div className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
-                        aria-expanded="true" aria-controls="collapseTwo">
-                        <i class="fa-solid fa-user-plus"></i>
-                        <span>Add User</span>
-                    </div>
-                </li>
-            </Link>
-
-            <Link to='/profile'>
-                <li className="nav-item">
-                    <div className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
-                        aria-expanded="true" aria-controls="collapseTwo">
-                        <i class="fa-solid fa-user"></i>
-                        <span>Profile</span>
-                    </div>
-                </li>
-            </Link>
-            <Link to='/pendingrequest'>
-                <li className="nav-item">
-                    <div className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
-                        aria-expanded="true" aria-controls="collapseTwo">
-                        <i class="fa-light fa-clipboard"></i>
-                        <span>Pending Request</span>
-                    </div>
-                </li>
-            </Link>
-           
-                <li className="nav-item" onClick={()=>navigate('/task')}>
-                    <div className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
-                        aria-expanded="true" aria-controls="collapseTwo">
-                        <i class="fa-sharp fa-solid fa-list fa-beat-fade"></i>
-                        <span>Task</span>
-                    </div>
-                </li>
-           
+            {/* <!-- Nav Item - Pages Collapse Menu --> */}
+            <li className="nav-item">
+                <a className="nav-link collapsed" href="javascript(void)" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i className="fa-solid fa-gear"></i>
+                    <span>Components</span>
+                </a>
+            </li>
         </ul>
-    );
+    </>
 }
 
-export default Sidebar;
+export default Sidebar
